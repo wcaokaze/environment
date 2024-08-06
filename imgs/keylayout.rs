926018fceba1a956358c2e18f00389eb4f4d465e
wcaokaze <wcaokaze@gmail.com>
2024-08-07 00:00:34 +0900

    白い正方形のSVGを出力してみた

--- imgs/keylayout.rs
+++ imgs/keylayout.rs
@@ -1,4 +1,26 @@
 #!/usr/bin/env -S cargo +nightly -Zscript
 
-fn main() {
+use std::io::{self, Write};
+use std::fs::File;
+
+const CANVAS_WIDTH:  usize = 300;
+const CANVAS_HEIGHT: usize = 300;
+
+fn main() -> io::Result<()> {
+   let mut svg_content = String::new();
+   svg_content.push_str(
+      &format!(r##"
+         <svg xmlns="http://www.w3.org/2000/svg"
+            width="{CANVAS_WIDTH}" height="{CANVAS_HEIGHT}"
+            viewBox="0 0 {CANVAS_WIDTH} {CANVAS_HEIGHT}">
+
+            <rect width="{CANVAS_WIDTH}" height="{CANVAS_HEIGHT}" fill="#fff"/>
+         </svg>
+      "##)
+   );
+
+   let mut file = File::create("keylayout.svg")?;
+   file.write_all(svg_content.as_bytes())?;
+
+   Ok(())
 }
